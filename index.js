cecc98c93b193ca8b2990cfde44e1826a3264d4b
mixzenin <mix.zenin.ifmo@yandex.ru>
2019-09-05 21:32:09 +0300

    Recursion replaced by async/await loop & add jsDoc documentation

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const etherscanBalances = require("./src/index");
 
 etherscanBalances
   .getMostChangedAddress(100)
+  .then(address => console.log(`Address with most changes is ${address}`))
   .catch(err => console.log(err));
--- src/etherscan/etherscan.js
+++ src/etherscan/etherscan.js
@@ -1,15 +1,25 @@
 const request = require("request");
 const ETHERSCAN_URL_API = require("./config").ETHERSCAN_URL_API;
 
+
+/** 
+ * @description Get last block of Ethereum blockchain
+ * @return {number} Number of last block
+ */ 
 let getLastBlockNumber = async () => {
   const qs = {
     module: "proxy",
     action: "eth_blockNumber"
   };
   const response = await callEtherscanApi(qs);
-  return response.result;
+  return parseInt(response.result, 16);
 };
 
+/** 
+ * @description Get all transactions in block
+ * @param {number} blockNumber Number of block for get transactions
+ * @return {Object[]} Transactions in block
+ */ 
 let getTransactionsInBlock = async blockNumber => {
   const qs = {
     module: "proxy",
@@ -21,6 +31,11 @@ let getTransactionsInBlock = async blockNumber => {
   return response.result.transactions
 };
 
+/** 
+ * @description Call Etherscan API
+ * @param {number} qs Params of API method
+ * @return {Object} Response of API
+ */ 
 let callEtherscanApi = qs => {
   return new Promise((resolve, reject) => {
     request(
